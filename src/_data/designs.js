ad240338df69e582e5fd24049ecd8d9ff3c21c35
chriskirknielsen <chriskirknielsen@gmail.com>
2024-05-18 19:06:13 -0400

    Update designs, add A11Y!

--- src/_data/designs.js
+++ src/_data/designs.js
@@ -8,7 +8,7 @@ const designs = [
 		links: {
 			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-CSS-by-ckirknielsen/72710420.6VDG0',
 			TeePublic: 'https://www.teepublic.com/t-shirt/19996533-retro-blank-vhs-css?store_id=49603',
-			Society6: 'https://society6.com/product/retro-blank-vhs-css_t-shirt?sku=s6-21377318p15a4v136a5v20a11v49',
+			Society6: 'https://society6.com/product/retro-blank-vhs-css_print?sku=s6-21377318p4a1v1',
 		},
 		tags: ['dev', 'www', 'css', 'vhs', 'retro'],
 		date: '2021-03-17',
@@ -22,7 +22,7 @@ const designs = [
 		links: {
 			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-HTML-by-ckirknielsen/73662806.CW2C9.XYZ',
 			TeePublic: 'https://www.teepublic.com/t-shirt/20289562-retro-blank-vhs-html?store_id=49603',
-			Society6: 'https://society6.com/product/retro-blank-vhs-html_t-shirt?sku=s6-21377261p15a4v97a5v18a11v49',
+			Society6: 'https://society6.com/product/retro-blank-vhs-html_print?sku=s6-21377261p4a1v1',
 		},
 		tags: ['dev', 'www', 'html', 'vhs', 'retro'],
 		date: '2021-03-17',
@@ -36,7 +36,7 @@ const designs = [
 		links: {
 			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-SVG-by-ckirknielsen/80893683.DYMRA',
 			TeePublic: 'https://www.teepublic.com/t-shirt/22687532-retro-blank-vhs-svg?store_id=49603',
-			Society6: 'https://society6.com/product/retro-blank-vhs-svg_t-shirt?sku=s6-21377348p15a4v136a5v18a11v49',
+			Society6: 'https://society6.com/product/retro-blank-vhs-svg8844315_print?sku=s6-28365206p4a1v1',
 		},
 		tags: ['dev', 'www', 'svg', 'vhs', 'retro'],
 		date: '2021-08-11',
@@ -50,7 +50,7 @@ const designs = [
 		links: {
 			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-JavaScript-ECMAScript-by-ckirknielsen/85189202.CW2C9.XYZ',
 			TeePublic: 'https://www.teepublic.com/t-shirt/23606086-retro-blank-vhs-javascript-ecmascript?store_id=49603',
-			Society6: 'https://society6.com/product/retro-blank-vhs-javascript-ecmascript_t-shirt?sku=s6-21377364p15a4v97a5v18a11v49',
+			Society6: 'https://society6.com/product/retro-blank-vhs-javascript-ecmascript_print?sku=s6-21377364p4a1v1',
 		},
 		tags: ['dev', 'www', 'js', 'vhs', 'retro'],
 		date: '2021-08-11',
@@ -66,11 +66,24 @@ const designs = [
 		links: {
 			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-JavaScript-ECMAScript-Dark-Variant-by-ckirknielsen/132703092.FB110.XYZ',
 			TeePublic: 'https://www.teepublic.com/t-shirt/36698589-retro-blank-vhs-javascript-ecmascript-dark-variant',
-			Society6: 'https://society6.com/product/retro-blank-vhs-javascript-ecmascript-dark-variant_t-shirt',
 		},
 		tags: ['dev', 'www', 'js', 'vhs', 'retro'],
 		date: '2022-11-20',
 		variant: ['vhs-js'],
+	},
+	{
+		slug: 'vhs-a11y',
+		name: 'Blank VHS A11Y',
+		description: 'In a world where the web runs on VHS tapes…',
+		note: 'For posters, RedBubble or Society6 are recommended',
+		img: 'vhs-a11y.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-A11Y-by-ckirknielsen/161318764.CW2C9?asc=u',
+			TeePublic: 'https://www.teepublic.com/t-shirt/60553454-retro-blank-vhs-a11y',
+			Society6: 'https://society6.com/product/retro-blank-vhs-a11y_print?sku=s6-28365165p4a1v1',
+		},
+		tags: ['dev', 'www', 'vhs', 'retro'],
+		date: '2024-05-18',
 	},
 	{
 		slug: 'vhs-www',
@@ -81,7 +94,6 @@ const designs = [
 		links: {
 			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-WWW-by-ckirknielsen/105322869.DYMRA?asc=u',
 			TeePublic: 'https://www.teepublic.com/t-shirt/28723641-retro-blank-vhs-www',
-			Society6: 'https://society6.com/product/retro-blank-vhs-www-ecmascript_t-shirt?sku=s6-23525463p15a4v75a5v20a11v49',
 		},
 		tags: ['dev', 'www', 'vhs', 'retro'],
 		date: '2022-03-20',
