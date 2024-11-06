83b9d13e6de54589b080830b7338698601037ef7
chriskirknielsen <chriskirknielsen@gmail.com>
2024-11-06 16:15:54 -0500

    Try out some anchor-positioning but revert
    Keeping the prototype code in there, gotta learn more about it first

--- .eleventy.js
+++ .eleventy.js
@@ -40,7 +40,7 @@ const assetsDir = `_assets`; // Assets folder
 const BUILD_CONTEXT = process?.env?.BUILD_CONTEXT || 'LIVE';
 const md = new markdownIt().disable('code');
 const purgeCssList = {
-	_global: { safe: [/^\:[-a-z]+$/, 'translated-rtl'], block: [] }, // Preserve any pseudo-class for now (thanks laurentpayot; still broken in 6.0 https://github.com/FullHuman/purgecss/issues/978)
+	_global: { safe: [/^\:[-a-z]+$/, 'translated-rtl', 'data-tooltip-pos'], block: [] }, // Preserve any pseudo-class for now (thanks laurentpayot; still broken in 6.0 https://github.com/FullHuman/purgecss/issues/978)
 	home: { safe: ['data-section=home'], block: ['data-section=about'] },
 	about: { safe: ['data-section=about'], block: ['data-section=home'] },
 };
