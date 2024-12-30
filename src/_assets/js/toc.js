d500d00895b6b6793ef4b2d63cfd0f25a5b2468d
chriskirknielsen <chriskirknielsen@gmail.com>
2024-12-29 22:48:44 -0500

    Little improvement to TOC highlight logic

--- src/_assets/js/toc.js
+++ src/_assets/js/toc.js
@@ -1,21 +1,23 @@
 // Basic (bad) "highlighter" of the TOC link for the current section in view
 document.addEventListener('DOMContentLoaded', () => {
 	const headings = document.querySelectorAll('.toc ~ h2 .heading-anchor');
-	const observer = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			console.log(entry.target.href, entry.time);
-			if (entry.isIntersecting) {
-				headings.forEach((h) => {
-					const tocLink = document.querySelector(`.toc-list a[href="${h.getAttribute('href')}"]`);
-					if (h === entry.target) {
-						tocLink.style.setProperty('--LINK-decoration-thickness', 'calc(var(--link-decoration-thickness, 1px) + 1.5px)');
-					} else {
-						tocLink.style.removeProperty('--LINK-decoration-thickness');
-					}
-				});
-			}
-		});
-	});
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					headings.forEach((h) => {
+						const tocLink = document.querySelector(`.toc-list a[href="${h.getAttribute('href')}"]`);
+						if (h === entry.target) {
+							tocLink.style.setProperty('--LINK-decoration-thickness', 'calc(var(--link-decoration-thickness, 1px) + 1.5px)');
+						} else {
+							tocLink.style.removeProperty('--LINK-decoration-thickness');
+						}
+					});
+				}
+			});
+		},
+		{ rootMargin: '0px 0px -33% 0px', threshold: 1 }
+	);
 
 	headings.forEach((heading) => observer.observe(heading));
 });
