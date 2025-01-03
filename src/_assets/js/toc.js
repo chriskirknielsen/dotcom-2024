7aec35b0929c49ed1a47007fa13ee37a5e498573
chriskirknielsen <chriskirknielsen@gmail.com>
2025-01-02 22:19:50 -0500

    Allow nested TOC highlighting

--- src/_assets/js/toc.js
+++ src/_assets/js/toc.js
@@ -1,29 +1,33 @@
 // Super very basic "highlighter" of the TOC link for the current section in view
 document.addEventListener('DOMContentLoaded', () => {
-	// In this site's layout, the table of contents is an element that appears before any other content at the same hierarchy,
-	// and level 2 headings are the only ones I want to target
-	const headings = Array.from(document.querySelectorAll('.toc ~ h2'));
+	// In this site's layout, the table of contents is an element that appears before any other content at the same hierarchy level
+	const headings = Array.from(document.querySelectorAll('.toc ~ :is(h2, h3, h4)'));
 	if (headings.length === 0) {
 		return; // No headings? No business here
 	}
 
-	const visibleHeadings = new Set();
-	const markTocItemActive = (a) => a.closest('.toc-list > li').setAttribute('data-current', '');
-	const markTocItemInactive = (a) => a.closest('.toc-list > li').removeAttribute('data-current');
+	// A few helper functions
+	const markTocItemActive = (a) => a.closest('.toc-list li').setAttribute('data-current', '');
+	const markTocItemInactive = (a) => a.closest('.toc-list li').removeAttribute('data-current');
 	const getTocLinkFromHeading = (h) => document.querySelector(`.toc-list a[href="#${h.id}"]`);
+	const getDocHeight = () => Math.floor(document.body.clientHeight);
 
-	// On page load we'll mark the first item as active, even if the heading appears a bit further down
-	markTocItemActive(getTocLinkFromHeading(headings[0]));
+	const visibleHeadings = new Set();
+	let resizeDebounce;
+	let currentObserver;
+	let height = getDocHeight();
 
-	const observer = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((entry) => {
-				// Keep track of visible headings
-				if (entry.isIntersecting) {
-					visibleHeadings.add(entry.target);
-				} else {
-					visibleHeadings.delete(entry.target);
-				}
+	function beginObservation(docHeight) {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					// Keep track of visible headings
+					if (entry.isIntersecting) {
+						visibleHeadings.add(entry.target);
+					} else {
+						visibleHeadings.delete(entry.target);
+					}
+				});
 
 				// Sort visible (intersecting) headings by inverted order of appearance, then grab the first item (i.e. last visible heading)
 				const lastVisible = Array.from(visibleHeadings.values()).sort((a, b) => headings.indexOf(b) - headings.indexOf(a))[0];
@@ -31,27 +35,47 @@ document.addEventListener('DOMContentLoaded', () => {
 					return; // If nothing is visible, weird — TOC are opt-in — but let's skip this logic
 				}
 
-				headings.forEach((h) => {
+				headings.forEach((heading) => {
 					// Find the link in the TOC list matching the heading in this list of h2 elements
-					const tocLink = getTocLinkFromHeading(h);
+					const tocLink = getTocLinkFromHeading(heading);
 
 					// If it's the last visible item, mark it to make it stand out, else, revert to the default style
-					if (h === lastVisible) {
+					if (heading === lastVisible) {
 						markTocItemActive(tocLink);
 					} else {
 						markTocItemInactive(tocLink);
 					}
 				});
-			});
-		},
-		{
-			//? clientHeight: Extend the detection above the heading so it's always considered as intersecting if above the current scrollport window
-			//? -33%: The element won't be considered as intersecting until it has gone _above_ the bottom third of the scrollport window
-			rootMargin: `${document.body.clientHeight}px 0px -33% 0px`,
-			threshold: 1,
-		}
-	);
+			},
+			{
+				//? h: Extend the detection above the heading so it's always considered as intersecting if above the scrollport
+				//? -33%: The element won't be considered as intersecting until it has gone _above_ the bottom third of the scrollport
+				rootMargin: `${docHeight}px 0px -33% 0px`,
+				threshold: 1, // Only considered intersecting if all the pixels are inside the intersection area
+			}
+		);
 
-	// And now… observe all the headings
-	headings.forEach((heading) => observer.observe(heading));
+		// And now… observe all the headings
+		headings.forEach((heading) => observer.observe(heading));
+
+		return observer;
+	}
+
+	// On page load...
+	markTocItemActive(getTocLinkFromHeading(headings[0])); // Mark the first item as active (even if the heading appears a bit further down)
+	currentObserver = beginObservation(height); // Start the intersection observer
+
+	// On resize, replace the observer with a new one matching the updated body height, if different
+	window.addEventListener('resize', () => {
+		clearTimeout(resizeDebounce);
+		resizeDebounce = setTimeout(() => {
+			const heightAfterResize = getDocHeight();
+			if (height !== heightAfterResize) {
+				if (currentObserver) {
+					currentObserver.disconnect();
+				}
+				currentObserver = beginObservation(heightAfterResize);
+			}
+		}, 200);
+	});
 });
