0dabc90dbe93ac40c051bcf34906259924444d65
chriskirknielsen <chriskirknielsen@gmail.com>
2025-01-22 23:36:54 -0500

    Minor TOC adjustment so unreached sections are not active

--- src/_assets/js/toc.js
+++ src/_assets/js/toc.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const getTocLinkFromHeading = (h) => document.querySelector(`.toc-list a[href="#${h.id}"]`);
 	const getDocHeight = () => Math.floor(document.body.clientHeight);
 
+	const entryThreshold = 0.5; // Decimal representation of the percentage of the viewport an element has to be within to be marked as intersecting
 	const visibleHeadings = new Set();
 	let resizeDebounce;
 	let currentObserver;
@@ -31,10 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 				// Sort visible (intersecting) headings by inverted order of appearance, then grab the first item (i.e. last visible heading)
 				const lastVisible = Array.from(visibleHeadings.values()).sort((a, b) => headings.indexOf(b) - headings.indexOf(a))[0];
-				if (!lastVisible) {
-					return; // If nothing is visible, weird — TOC are opt-in — but let's skip this logic
-				}
-
 				headings.forEach((heading) => {
 					// Find the link in the TOC list matching the heading in this list of heading elements
 					const tocLink = getTocLinkFromHeading(heading);
@@ -50,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			{
 				//? h: Extend the detection above the heading so it's always considered as intersecting if above the scrollport
 				//? -33%: The element won't be considered as intersecting until it has gone _above_ the bottom third of the scrollport
-				rootMargin: `${docHeight}px 0px -33% 0px`,
+				rootMargin: `${docHeight}px 0px -${entryThreshold * 100}% 0px`,
 				threshold: 1, // Only considered intersecting if all the pixels are inside the intersection area
 			}
 		);
@@ -61,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// On page load...
-	markTocItemActive(getTocLinkFromHeading(headings[0])); // Mark the first item as active (even if the heading appears a bit further down)
+	// markTocItemActive(getTocLinkFromHeading(headings[0])); // Mark the first item as active (even if the heading appears a bit further down)
 	currentObserver = beginObservation(height); // Start the intersection observer
 
 	// On resize, replace the observer with a new one matching the updated body height, if different
