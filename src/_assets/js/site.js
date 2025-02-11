37cde3871b3fa94c4f9e0fe5808bfb3a8b88612e
chriskirknielsen <chriskirknielsen@gmail.com>
2025-02-11 09:02:18 -0500

    Add pointless-but-fun console message

--- src/_assets/js/site.js
+++ src/_assets/js/site.js
@@ -27,3 +27,32 @@ window.matchMedia(`(min-width:${globalBreakpoint})`).addEventListener('change', 
 	Array.from(document.body.querySelectorAll('.header-menu-toggle, .header-themepicker-toggle')).forEach((el) => el.setAttribute('aria-pressed', 'false'));
 	toggleInertForMenu(false);
 });
+
+console.log(`
+           Howdy fellow explorer of the Internet.
+                 Thanks for stoppin' on by.
+
+
+-----------------------------♥︎------------------------------
+
+
+     CCCCCCCCCCCCCCCCC        KKKKKKKK NNNNNNNNNNNNNNNN     
+  CCCCCCCCCCCCCCCCCCCC      KKKKKKKK   NNNNNNNNNNNNNNNNNNN  
+ CCCCCCCCCCCCCCCCCCCCC    KKKKKKKK     NNNNNNNNNNNNNNNNNNNN 
+CCCCCCC          CCCCC  KKKKKKKK       NNNNNN        NNNNNNN
+CCCCCC           CCC  KKKKKKKK         NNNNNN         NNNNNN
+CCCCCC              KKKKKKKK           NNNNNN         NNNNNN
+CCCCCC             KKKKKKKK            NNNNNN         NNNNNN
+CCCCCC              KKKKKKKK           NNNNNN         NNNNNN
+CCCCCC           CCC  KKKKKKKK         NNNNNN         NNNNNN
+CCCCCCC          CCCCC  KKKKKKKK       NNNNNN         NNNNNN
+ CCCCCCCCCCCCCCCCCCCCC    KKKKKKKK     NNNNNN         NNNNNN
+  CCCCCCCCCCCCCCCCCCCC      KKKKKKKK   NNNNNN         NNNNNN
+     CCCCCCCCCCCCCCCCC       KKKKKKKKK NNNNNN         NNNNNN
+
+	 
+-----------------------------!------------------------------
+
+
+This place is not a place of honor...no highly esteemed deed
+       is commemorated here...nothing valued is here.`);
