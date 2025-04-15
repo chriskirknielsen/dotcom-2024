b06063ea4205b0363040f6960b7e1d3a97bdad13
chriskirknielsen <chriskirknielsen@gmail.com>
2025-04-14 21:50:02 -0400

    Redirect /merch to /designs just in case?

--- src/_includes/assets/js/site.js
+++ src/_includes/assets/js/site.js
@@ -1,16 +1,16 @@
 (()=>{function a(t=!1){Array.from(document.body.querySelectorAll(":scope > :not(header)")).forEach(e=>e.inert=t)}document.addEventListener("click",function(t){let e;if(e=t.target.closest("[data-toggle-pressed]")){let r=e.getAttribute("aria-pressed")!=="true";e.setAttribute("aria-pressed",r.toString()),e.matches(".header-menu-toggle")&&a(r)}else e=t.target,e.closest(".header-themepicker")||document.querySelector(".header-themepicker-toggle").setAttribute("aria-pressed","false")});document.addEventListener("keyup",function(t){let e=document.querySelector('[data-toggle-pressed][aria-pressed="true"]');e&&(t.key==="Escape"||t.keyCode===27)&&(e.setAttribute("aria-pressed","false"),a(!1),e.focus())});document.addEventListener("mouseenter",function(t){let e=t?.target?.closest?.(".footer-message");e&&(e.classList.add("activated"),e.addEventListener("animationend",r=>e.classList.remove("activated"),{once:!0}))},{capture:!0});window.matchMedia(`(min-width:${globalBreakpoint})`).addEventListener("change",function(t){Array.from(document.querySelectorAll(".header-menu-toggle, .header-themepicker-toggle")).forEach(e=>e.setAttribute("aria-pressed","false")),a(!1)});console.log(`
-	To meet a fellow explorer
-	In these liminal spaces
-	Outside hyperlink anchors
-	Hah, what a nice surprise
+To meet a fellow explorer
+In these liminal spaces
+Outside hyperlink anchors
+Hah, what a nice surprise
 
-	I hope not to take much
-	Of your attention any further
-	But hope you will keep an eye out
-	For what is worthy to discover
+I hope not to take much
+Of your attention any further
+But hope you will keep an eye out
+For what is worthy to discover
 
-	I wish your quest leads you farther
-	Someplace you can feel safe, and yet
-	Where you'd still want whisper:
-	\u201CHah, what a nice surprise.\u201D
+I wish your quest leads you farther
+Someplace you can feel safe, and yet
+Where you'd still want whisper:
+\u201CHah, what a nice surprise.\u201D
 `.trim());})();
