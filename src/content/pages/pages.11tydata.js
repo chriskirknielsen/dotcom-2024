316eb923424d85f1a14a4838b6070fe5e81f8b27
chriskirknielsen <chriskirknielsen@gmail.com>
2025-05-31 16:26:03 -0400

    Fix remaining Vento issues

--- src/content/pages/pages.11tydata.js
+++ src/content/pages/pages.11tydata.js
@@ -2,7 +2,5 @@ export default {
 	tags: ['_pages'],
 	section: 'page',
 	permalink: (data) => `/${data.page.fileSlug}/index.html`,
-	eleventyComputed: {
-		layout: (data) => `page.${data.page.inputPath.endsWith('vto') ? 'vto' : 'njk'}`,
-	},
+	layout: 'page.vto',
 };
