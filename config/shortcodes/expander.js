4dab2cdb15dc24e06d07b32470653f366633d83f
chriskirknielsen <chriskirknielsen@gmail.com>
2025-06-07 17:22:23 -0400

    Adjust the expander component to use a config object

--- config/shortcodes/expander.js
+++ config/shortcodes/expander.js
@@ -1,8 +1,9 @@
 export default function (eleventyConfig, options = {}) {
-	eleventyConfig.addPairedShortcode('expander', function (content, title = 'Expand', showArrow = true, isExpanded = false) {
+	eleventyConfig.addPairedShortcode('expander', function (content, title = 'Expand', config = {}) {
+		const { showArrow, isExpanded, summaryId } = Object.assign({ showArrow: true, isExpanded: false, summaryId: null }, config);
 		const arrow = showArrow ? `<span class="expander-button-arrow" aria-hidden="true"></span> ` : '';
 		return `<details class="expander"${isExpanded ? ' open' : ''}>
-			<summary class="expander-button | button | inline-size-100pc">${arrow}${title}</summary>
+			<summary class="expander-button | button | inline-size-100pc"${summaryId ? ` id="${summaryId}"` : ''}>${arrow}${title}</summary>
 			<div class="expander-content | flow">${content}</div>
 		</details>`;
 	});
