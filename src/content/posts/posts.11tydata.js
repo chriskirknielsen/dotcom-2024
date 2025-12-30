eb7c34fd1fe6ebd8605c02ddaa9c54a9716c61a6
chriskirknielsen <chriskirknielsen@gmail.com>
2025-12-29 23:56:15 -0500

    Slugify shenanigans

--- src/content/posts/posts.11tydata.js
+++ src/content/posts/posts.11tydata.js
@@ -7,7 +7,7 @@ export default {
 		date: function (data) {
 			const date = new Date(data.date || data.page.date);
 			if (data.time) {
-				const datetime = new Date(date.toISOString().replace('00:00:00', data.time));
+				const datetime = new Date(date.toISOString().replace('00:00:00', data.time)); // A little faster than individually replacing hours/minutes/seconds
 				if (datetime !== 'Invalid DateTime') {
 					return datetime;
 				}
@@ -18,7 +18,19 @@ export default {
 			return new Date(data.date || data.page.date).getFullYear();
 		},
 		permalink: function (data) {
-			return `blog/${this.slugify(data.slug || data.page.fileSlug)}/index.html`;
+			let baseSlug = data.slug; // By default, use the provided slug, if any (considered a hardcoded override, so no other logic should run if this exists)
+
+			if (!baseSlug) {
+				baseSlug = data.page.fileSlug; // Fall back to the computed file slug (date prefix is discarded)
+				const postYMD = (data.date || data.page.date).toISOString().split('T').shift(); // Extract the Y-M-D date of the post
+
+				// If the post slug is equal to the date, it's likely a containing folder without a Y-M-D-slug format, so we can use the title as a base string instead
+				if (postYMD === data.page.fileSlug) {
+					baseSlug = data.title; // This gets slugified, it's fine
+				}
+			}
+
+			return `blog/${this.slugify(baseSlug)}/index.html`; // Build the permalink by slugifying the final string used a slug, to ensure it is safe to use
 		},
 		customMetaImage: function (data) {
 			if (data.permalink) {
