594b85387c42ae5661a5105dda0ab248d1c17305
chriskirknielsen <chriskirknielsen@gmail.com>
2026-02-14 10:10:36 -0500

    Remove compiled asset files

--- src/_includes/assets/js/theme-set.js
+++
@@ -1,1 +0,0 @@
-(()=>{window.themeStore="cknTheme";var e=localStorage.getItem(window.themeStore);window.themeKeys.includes(e)?docEl.setAttribute("data-theme",e):localStorage.removeItem(window.themeStore);})();
