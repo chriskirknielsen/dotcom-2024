0ae5626ea6b79fea60fbfb201a292416f117aeb5
chriskirknielsen <chriskirknielsen@gmail.com>
2026-03-30 08:37:58 -0400

    Fix expander

--- config/shortcodes/expander.js
+++ config/shortcodes/expander.js
@@ -2,9 +2,11 @@ export default function (eleventyConfig, options = {}) {
 	eleventyConfig.addPairedShortcode('expander', function (content, title = 'Expand', config = {}) {
 		const { showArrow, isExpanded, summaryId } = Object.assign({ showArrow: true, isExpanded: false, summaryId: null }, config);
 		const arrow = showArrow ? `<span class="expander-button-arrow" aria-hidden="true"></span> ` : '';
+		//! Adding an extra line before the content output so Markdown doesn't get messed up
 		return `<details class="expander"${isExpanded ? ' open' : ''}>
 			<summary class="expander-button | button | inline-size-100pc"${summaryId ? ` id="${summaryId}"` : ''}>${arrow}${title}</summary>
-			<div class="expander-content | flow">${content}</div>
+			<div class="expander-content | flow">
+			${content}</div>
 		</details>`;
 	});
 }
