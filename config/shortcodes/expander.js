9f38897d4f9686cfe70b7d0673ccd146461155bf
chriskirknielsen <chriskirknielsen@gmail.com>
2026-05-16 14:05:58 -0400

    Play around with Expander arrows

--- config/shortcodes/expander.js
+++ config/shortcodes/expander.js
@@ -1,7 +1,9 @@
 export default function (eleventyConfig, options = {}) {
 	eleventyConfig.addPairedShortcode('expander', function (content, title = 'Expand', config = {}) {
 		const { showArrow, isExpanded, summaryId } = Object.assign({ showArrow: true, isExpanded: false, summaryId: null }, config);
-		const arrow = showArrow ? `<span class="expander-button-arrow" aria-hidden="true"></span> ` : '';
+		const arrow = showArrow
+			? `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" class="expander-button-arrow" aria-hidden="true"><path d="M9 5 3 1.54v6.92z" /></svg> `
+			: '';
 		//! Adding an extra line before the content output so Markdown doesn't get messed up
 		return `<details class="expander"${isExpanded ? ' open' : ''}>
 			<summary class="expander-button | button | inline-size-100pc"${summaryId ? ` id="${summaryId}"` : ''}>${arrow}${title}</summary>
