2512ff6d53e5dcc4dc43f15daba90226d246e97b
chriskirknielsen <chriskirknielsen@gmail.com>
2026-06-19 21:28:32 -0400

    Allow the expander to have a name on the underlying details element

--- config/shortcodes/expander.js
+++ config/shortcodes/expander.js
@@ -1,11 +1,11 @@
 export default function (eleventyConfig, options = {}) {
 	eleventyConfig.addPairedShortcode('expander', function (content, title = 'Expand', config = {}) {
-		const { showArrow, isExpanded, summaryId } = Object.assign({ showArrow: true, isExpanded: false, summaryId: null }, config);
+		const { showArrow, isExpanded, summaryId, name } = Object.assign({ showArrow: true, isExpanded: false, summaryId: null, name: null }, config);
 		const arrow = showArrow
 			? `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" class="expander-button-arrow" aria-hidden="true"><path d="M9 5 3 1.54v6.92z" /></svg> `
 			: '';
 		//! Adding an extra line before the content output so Markdown doesn't get messed up
-		return `<details class="expander"${isExpanded ? ' open' : ''}>
+		return `<details class="expander"${isExpanded ? ' open' : ''}${name ? ` name="${name}"` : ''}>
 			<summary class="expander-button | button | inline-size-100pc"${summaryId ? ` id="${summaryId}"` : ''}>${arrow}${title}</summary>
 			<div class="expander-content | flow">
 			${content}</div>
