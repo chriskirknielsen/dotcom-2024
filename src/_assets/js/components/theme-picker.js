206e02721a2fcd8ec73c53ba25c66948f96dc746
chriskirknielsen <chriskirknielsen@gmail.com>
2026-09-04 22:40:34 -0400

    Fix issue where some keypresses were incorrectly matched as a number

--- src/_assets/js/components/theme-picker.js
+++ src/_assets/js/components/theme-picker.js
@@ -267,14 +267,16 @@ class ThemePicker extends HTMLElement {
 
 		if (e.type === 'keyup') {
 			const isModifierPressed = e.altKey || e.shiftKey || e.ctrlKey || e.metaKey;
+			const pressedDigit = parseInt(e.key, 10); // Gotta parse it first so ' ' isn't matched for `' ' >= 0`, sadly
+			const isValidDigitKey = !isNaN(pressedDigit) && pressedDigit >= 0 && pressedDigit <= this.keys.length;
+
 			if (e.key === 'Escape' && !isModifierPressed) {
 				const themePickerToggleButton = document.querySelector(`[aria-controls=${this.id}]`);
 				if (themePickerToggleButton && themePickerToggleButton.getAttribute('aria-pressed') === 'true') {
 					themePickerToggleButton.setAttribute('aria-pressed', 'false');
 					themePickerToggleButton.focus(); // Restore focus to the toggler
 				}
-			} else if (e.key >= 0 && e.key <= this.keys.length && !e.target?.closest?.('input, textarea, [contenteditable]') && !isModifierPressed) {
-				const pressedDigit = parseInt(e.key, 10);
+			} else if (isValidDigitKey && !e.target?.closest?.('input, textarea, [contenteditable]') && !isModifierPressed) {
 				this.save(this.keys[pressedDigit - 1] || ''); // For pressedDigit === 0, we get an index of -1, which is undefined, so the fallback of empty string gives us the system default
 			}
 		}
